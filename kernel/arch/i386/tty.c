6ae021e739fb81b5671f0de740152b5264149e95
Raaid Tanveer <raaidraiyantanveer@gmail.com>
2024-06-02 01:34:07 -0400

    Renders newlines, and the Cursor adjusts to the correct position

--- kernel/arch/i386/tty.c
+++ kernel/arch/i386/tty.c
@@ -6,6 +6,15 @@
 #include <kernel/tty.h>
 
 #include "vga.h"
+#include "io.h"
+
+/* The I/O ports */
+#define FB_COMMAND_PORT         0x3D4
+#define FB_DATA_PORT            0x3D5
+
+/* The I/O port commands */
+#define FB_HIGH_BYTE_COMMAND    14
+#define FB_LOW_BYTE_COMMAND     15
 
 /* 
  * This is an example of memory-mapped I/O. 
@@ -45,20 +54,40 @@ void terminal_putentryat(unsigned char c, uint8_t color, size_t x, size_t y) {
 }
 
 void terminal_putchar(char c) {
-	unsigned char uc = c;
-	terminal_putentryat(uc, terminal_color, terminal_column, terminal_row);
-	if (++terminal_column == VGA_WIDTH) {
-		terminal_column = 0;
-		if (++terminal_row == VGA_HEIGHT)
+	unsigned char uc;
+	switch (c) {
+	case '\n':
+		if (++terminal_row == VGA_HEIGHT) 
 			terminal_row = 0;
+		terminal_column = 0;
+		break;
+	default:
+		uc = c;
+		terminal_putentryat(uc, terminal_color, terminal_column, terminal_row);
+		if (++terminal_column == VGA_WIDTH) {
+			terminal_column = 0;
+			if (++terminal_row == VGA_HEIGHT)
+				terminal_row = 0;
+		}
 	}
+	
 }
 
 void terminal_write(const char* data, size_t size) {
-	for (size_t i = 0; i < size; i++)
+	for (size_t i = 0; i < size; i++) {
 		terminal_putchar(data[i]);
+		move_cursor(terminal_row, terminal_column);
+	}
 }
 
 void terminal_writestring(const char* data) {
 	terminal_write(data, strlen(data));
+}
+
+void move_cursor(unsigned char row, unsigned char col) {
+	unsigned short pos = ((unsigned short) row) * VGA_WIDTH + ((unsigned short) col);
+	outb(FB_COMMAND_PORT, FB_HIGH_BYTE_COMMAND);
+	outb(FB_DATA_PORT,    ((pos >> 8) & 0x00FF));
+	outb(FB_COMMAND_PORT, FB_LOW_BYTE_COMMAND);
+	outb(FB_DATA_PORT,    pos & 0x00FF);
 }
--- kernel/kernel/kernel.c
+++ kernel/kernel/kernel.c
@@ -2,7 +2,9 @@
 
 #include <kernel/tty.h>
 
+#define __is_myos_kernel 1
+
 void kernel_main(void) {
 	terminal_initialize();
-	printf("Hello, %s World %d!\n", "kernel", 42);
+	printf("Hello, %s World %d!\nHere is another line\n", "kernel", 42);
 }
