493a0db1438a0dd5c3946284b69231ac0bbb4e72
Raaid Tanveer <raaidraiyantanveer@gmail.com>
2024-06-03 21:05:51 -0400

    Added a command line option for qemu.sh to enable logging on the serial port, and added an inb instruction accessible through C

--- qemu.sh
+++ qemu.sh
@@ -2,4 +2,30 @@
 set -e
 . ./iso.sh
 
-qemu-system-$(./target-triplet-to-arch.sh $HOST) -cdrom myos.iso
+while getopts ":d:h" opt; do
+  case $opt in
+    d)
+      if [ -z "$OPTARG" ]; then 
+        qemu-system-$(./target-triplet-to-arch.sh $HOST) -serial file:logfile.log -cdrom myos.iso
+      else 
+        qemu-system-$(./target-triplet-to-arch.sh $HOST) -serial file:"$OPTARG" -cdrom myos.iso
+      fi
+      ;;
+    h) 
+      echo "Usage: ./qemu.sh [OPTIONS]"
+      echo "OPTIONS:"
+      echo "  -d [logfile] Starts logging to a specified logfile."
+      echo "               If no logfile is specified, logs to log.txt."
+      echo "  -h           Prints this help page."
+      ;;
+    \?)
+      echo "Invalid option: -$OPTARG" >&2
+      exit 1
+      ;;
+  esac
+done
+
+
+if [ $OPTIND -eq 1 ]; then
+  qemu-system-$(./target-triplet-to-arch.sh $HOST) -cdrom myos.iso
+fi
